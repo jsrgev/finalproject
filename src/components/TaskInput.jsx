786e67b22b940dcc13940c712736f5acb2689e6b
Josh Regev <jsrgev@gmail.com>
2021-06-20 17:11:37 +0300

    tweaked task input style a little bit

--- src/components/TaskInput.jsx
+++ src/components/TaskInput.jsx
@@ -4,8 +4,8 @@ class TaskInput extends React.Component {
 	render () {
 		return (
 			<div id="taskInput">
-				<input placeholder="New item" />
-				<input placeholder="Finish" />
+				<input placeholder="New item" className="inputTaskName" />
+				<input placeholder="Finish" className="inputTaskFinish" />
 				<i className="fas fa-plus"></i>
 			</div>
 		)
