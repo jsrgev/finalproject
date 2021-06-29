8d792eccda5474819ad545937c2e8acdea99267e
Josh Regev <jsrgev@gmail.com>
2021-06-29 08:54:36 +0300

    added redux-devtools-extension to try to solve safari - firefox problem

--- src/index.js
+++ src/index.js
@@ -5,15 +5,38 @@ import App from './App.jsx';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from "react-router-dom";
 import thunk from 'redux-thunk'
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 import {createStore, applyMiddleware, compose} from 'redux';
 import {Provider} from 'react-redux';
 
 import {reducer} from './redux/reducer.js';
 
-const store = createStore(reducer, compose(applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ && window.__REDUX_DEVTOOLS_EXTENSION__()));
+const middleware = [thunk];
+
+// const composeEnhancers = composeWithDevTools({
+//   // options like actionSanitizer, stateSanitizer
+// });
+
+// const store = createStore(
+//   reducer, compose(applyMiddleware(thunk),
+//     window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+//   );
+
+// const store = createStore(
+//   reducer,
+//   /* preloadedState, */
+//   composeEnhancers(
+//   applyMiddleware(...middleware),
+//   // other store enhancers if any
+// ));
 
 
+const store = createStore(
+  reducer,
+  // initialState,
+  composeWithDevTools(applyMiddleware(...middleware))
+);
 
 ReactDOM.render(
   <React.StrictMode>
